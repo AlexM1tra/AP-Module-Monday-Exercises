e2d52ec4ac2a1618463f0cb99acfd1c4e3f71c76
AlexM1tra <56548696+AlexM1tra@users.noreply.github.com>
2021-01-11 15:23:44 +0000

    Update exercise4.cpp
    Updated so that the program works with decimal numbers.

--- exercise4.cpp
+++ exercise4.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
 
+bool isEven(char digit) {
+  std::string evens = "02468";
+  if (evens.find(digit) != std::string::npos) {
+    return true;
+  }
+  return false;
+}
+
+char lastDigit(std::string number) {
+  return number[number.size()-1]; 
+}
+
 int main() {
-  int number;
+  std::string number;
   std::cout << "Please enter a value? ";
   std::cin >> number;
-  if (number % 2 == 0) {
+  if (isEven(lastDigit(number))) {
     std::cout << number << " is an even number" << std::endl;
   } else {
     std::cout << number << " is an odd number" << std::endl;
