9c13d6721b5a001c9189fd1a81b767dd341d6fcc
AlexM1tra <56548696+AlexM1tra@users.noreply.github.com>
2021-01-11 20:28:18 +0000

    Update exercise9.cpp
    Adds validation and documentation for exercise 9

--- exercise9.cpp
+++ exercise9.cpp
@@ -1,22 +1,29 @@
 #include <iostream>
 
+// Validates user's input and returns valid char.
+char charInput() {
+  std::string input;
+  std::cin >> input;
+  while (input.size() > 1) {
+    std::cout << "Please enter only one character at a time: ";
+    std::cin >> input;
+  }
+  return input[0];
+}
+
 int main() {
-  char character;
-  std::cout << "Please enter a character? ";
-  std::cin >> character;
-  if (isalpha(character)) {
-    std::cout << "Alphabetic character detected.\n";
-  } else if (isdigit(character)) {
-    std::cout << "Digit character detected.\n";
-  } else {
-    std::cout << "Special character detected.\n";
-  }
-  char tryAgain;
-  std::cout << "Try again (y/N)? ";
-  std::cin >> tryAgain;
-  if (toupper(tryAgain) != 'Y') {
-    std::cout << "Thank you, good bye.\n";
-  } else {
-    main();
-  }
+  char character, tryAgain;
+  do {
+    std::cout << "Please enter a character? ";
+    character = charInput();
+    if (isalpha(character)) {
+      std::cout << "Alphabetic character detected.\n";
+    } else if (isdigit(character)) {
+      std::cout << "Digit character detected.\n";
+    } else {
+      std::cout << "Special character detected.\n";
+    }
+    std::cout << "Try again (Y/N)? ";
+    tryAgain = charInput();
+  } while (toupper(tryAgain) == 'Y');
 }
